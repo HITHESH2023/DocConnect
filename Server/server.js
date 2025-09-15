3a4d09e2a22faefe44198093d5d3911c9a26fca7
HITHESH2023 <149927521+HITHESH2023@users.noreply.github.com>
2025-09-15 20:52:11 +0530

    Update server.js

--- Server/server.js
+++ Server/server.js
@@ -20,11 +20,27 @@ const app = express();
 app.use(express.json());
 
 const cors = require('cors');
-app.use(cors({
-  origin: 'http://localhost:3000',
+
+const allowedOrigins = [
+  'http://localhost:3000',
+  'https://docconnect-client.onrender.com'
+];
+
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (!origin) return callback(null, true);
+
+    if (allowedOrigins.indexOf(origin) === -1) {
+      const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  },
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
-}));
+};
+
+app.use(cors(corsOptions));
 
 // Use routes
 app.use("/api/auth", authRoutes);
